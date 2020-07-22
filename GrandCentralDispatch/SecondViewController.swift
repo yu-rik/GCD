f7274680b931bb03a7035b69a7acbf4b516149dd
Yurii Nosachenko <yurik_n@outlook.com>
2020-07-22 16:59:51 +0300

    Download image and added activity indicator

--- GrandCentralDispatch/SecondViewController.swift
+++ GrandCentralDispatch/SecondViewController.swift
@@ -10,12 +10,50 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get{
+            return imageView.image
+        }
+        set{
+            
+            activityIndicator.startAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fetchImage()
         // Do any additional setup after loading the view.
     }
     
+    //метод для загрузки изображения
+    fileprivate func fetchImage () {
+        imageURL = URL(string: "https://thumbs.dreamstime.com/z/bright-balls-14145086.jpg")
+       //перед загрузкой запускаем activityIndicator
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        guard let url = imageURL, let imageData = try? Data (contentsOf: url) else {return}
+        self.image = UIImage(data: imageData)
+     
+        /* if let url = imageURL {
+            let imageData = try? Data(contentsOf: url)
+            if imageData != nil{
+                self.image = UIImage(data: imageData!)
+            } else {
+                self.image = nil
+            }
+        }
+  */
+    }
 
     /*
     // MARK: - Navigation
