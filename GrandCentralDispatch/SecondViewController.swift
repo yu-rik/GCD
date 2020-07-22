65544da90001ae0b0c489a7c7bebd12b9b1f2886
Yurii Nosachenko <yurik_n@outlook.com>
2020-07-22 22:43:57 +0300

    Dispatch After( задержка)

--- GrandCentralDispatch/SecondViewController.swift
+++ GrandCentralDispatch/SecondViewController.swift
@@ -27,14 +27,49 @@ class SecondViewController: UIViewController {
         }
     }
     
+    //функция Алерт-регистрации
+    fileprivate func alertLogin() {
+        //создание Алерт-контроллера
+        let ac = UIAlertController(title: "Зарегистрированы?", message: "Введите логин и пароль:", preferredStyle: .alert)
+        
+        //добавление кнопок
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelButton = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        //добавление кнопок к алерт-контроллеру
+        ac.addAction(okButton)
+        ac.addAction(cancelButton)
+        
+        //добавление текстовых полей к алерт-контроллеру
+        ac.addTextField { (userLoginTF) in
+            userLoginTF.placeholder = "Введите логин"
+        }
+        ac.addTextField { (userPasswordTF) in
+            userPasswordTF.placeholder = "Введите пароль"
+            userPasswordTF.isSecureTextEntry = true
+        }
+        //отображение алертКонтроллера
+        self.present(ac, animated: true, completion: nil)
+        
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchImage()
+        delay(3) {
+            self.alertLogin()
+            
+        }
         // Do any additional setup after loading the view.
     }
     
+    //метод для задержки потока
+    fileprivate func delay(_ delay: Int, closure: @escaping ()->()){
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+    }
     //метод для загрузки изображения
     fileprivate func fetchImage () {
         imageURL = URL(string: "https://i.pinimg.com/originals/ff/c8/80/ffc880cba3e810d0a216f493531fdcf8.png")
